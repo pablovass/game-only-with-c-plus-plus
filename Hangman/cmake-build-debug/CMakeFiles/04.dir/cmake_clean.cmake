1661636d9fff9019171b68d556e26eaea1f20bd7
pablovass <faktory.fm@live.com>
2022-05-25 11:53:46 -0300

    hangman

---
+++ Hangman/cmake-build-debug/CMakeFiles/04.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "04.exe"
+  "04.exe.manifest"
+  "04.lib"
+  "04.pdb"
+  "CMakeFiles/04.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Hangman/cmake-build-debug/CMakeFiles/Hangman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hangman.dir/main.cpp.obj"
+  "Hangman.exe"
+  "Hangman.exe.manifest"
+  "Hangman.lib"
+  "Hangman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
